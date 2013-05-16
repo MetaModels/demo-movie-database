13bb8295a4aca55ab92be7b9da1bddae7ae0a4c2
Andreas Isaak <andy.jared@googlemail.com>
2013-05-16 11:02:45 +0200

    Add settings

---
+++ a-movie-database/system/modules/movie-database/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   Movie Database
+ * @subpackage MetaModels
+ * @author     Andreas Isaak <info@andreas-isaak.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+$GLOBALS['TL_CONFIG']['useAutoItem']                = true;
+$GLOBALS['TL_CONFIG']['urlSuffix']                  = '';
