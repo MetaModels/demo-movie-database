12d6f4689b4be7f0070023c7c1c83846d5df4b7e
Mini Model <minimodel@metamodel.me>
2013-10-15 00:18:44 +0200

    Updated translations from transifex.

---
+++ system/modules/mm_demo_movie_database/languages/rm/mm_demo_movie_database.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:03+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['description']  = 'En questa demonstraziun vegns ti a vesair sco che ti pos crear ina glista da films cun mussar ils trailers (via youtube u vimeo) ed ils covers (via amazon). Ils acturs che giogan en il film ed ils reschissurs èn mintgamain in MetaModel seperà, collià entras in champ da select e da tag.';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['headline']     = 'Demonstraziun Live da MetaModels';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['installed']    = 'Las infurmaziuns d\'access èn vegnidas redefinidas a "k.jones/kevinjones", ti es ussa annunzià sco quest udilisader. Nus ta giavischain bun temp durant explorar questa demo e grazia fitg per utilisar MetaModels.';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['official_url'] = 'http://now.metamodel.me/a-movie-database';
