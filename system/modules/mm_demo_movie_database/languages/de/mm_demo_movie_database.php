a260c6d218ea80bf9015432b4fa76d3ea827fcf6
Mini Model <minimodel@metamodel.me>
2013-10-15 16:14:31 +0200

    Updated translations from transifex.

---
+++ system/modules/mm_demo_movie_database/languages/de/mm_demo_movie_database.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:03+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['description']  = 'In dieser Demo lernen Sie, wie eine Auflistung von Filmen gemeinsam mit der Anzeige von Trailern (von YouTube und Vimeo) und Titelbildern (von Amazon) erstellt wird. Die Daten zu SchauspielerInnen und RegisseurInnen sind separate MetaModels, die über Select- und Tagfelder verlinkt sind.';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['headline']     = 'MetaModels Live-Demo';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['installed']    = 'Die Benutzerdaten wurden zu "k.jones/kevinjones" zurückgesetzt. Diese Daten haben Ihren eingeloggten Benutzer ersetzt. Vielen Dank für die Benutzung von MetaModels - wir wünschen viel Freude bei der Erkundung der Demo.';
+$GLOBALS['TL_LANG']['mm_demo_movie_database']['official_url'] = 'http://now.metamodel.me/a-movie-database';
---
+++ system/modules/mm_demo_movie_database/languages/de/modules.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:03+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['mm_demo_movie_database']['0'] = 'Movie-Datenbank (Demo)';
+$GLOBALS['TL_LANG']['MOD']['mm_demo_movie_database']['1'] = 'Die offizielle Movie-Datenbank-Demo';
